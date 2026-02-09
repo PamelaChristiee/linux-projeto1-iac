ba8409a388e943cec0ae085c62f98140f9425464
Pamela Christie <pamelachristie99@gmail.com>
2026-02-09 19:59:00 -0300

    Adciona script de criação de usuários

---
+++ iac1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd paloma -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd lucas -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd jorge -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd rosimere -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd rose -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissões dos diretórios...."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim....."
